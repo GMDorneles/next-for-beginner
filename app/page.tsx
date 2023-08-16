14c8617c7fef1d77973a3e943d0333904030f369
Gabrielly Maldonado <gabrielly.maldonadoo@gmail.com>
2023-08-16 09:31:34 -0400

    style:  add css

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,20 @@ export default function Home() {
   };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div>
-        <h1>Your Name</h1>
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">Your Name</h1>
+
+        <form onSubmit={handleSubmit} className="space-y-3">
+          <input
+            className="w-full p-2 border border-gray-300 rounded text-black"
+            type="text"
+            placeholder="Type your name"
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}
+          />
+          <button type="submit">Predict Data</button>
+        </form>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Type your name"
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-        />
-        <button type="submit">Predict Data</button>
-      </form>
     </div>
   );
 }
--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,15 +1,15 @@
 const getPredictedAge = async (name: string) => {
-  const res = await fetch(`http//api.agify.io/name=${name}`);
+  const res = await fetch(`https://api.agify.io?name=${name}`);
   return res.json();
 };
 
 const getPredictedGender = async (name: string) => {
-  const res = await fetch(`http//api.genderize.io/name=${name}`);
+  const res = await fetch(`https://api.genderize.io?name=${name}`);
   return res.json();
 };
 
 const getPredictedCountry = async (name: string) => {
-  const res = await fetch(`http//api.nationalize.io/name=${name}`);
+  const res = await fetch(`https://api.nationalize.io?name=${name}`);
   return res.json();
 };
 
@@ -27,10 +27,13 @@ export default async function Page({ params }: Params) {
     countryData,
   ]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      {' '}
-      {/* get from url */}
-      {params.name}
-    </main>
+    <div  className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
+      <div className="p-8">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Personal info</div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">Age:{age?.age}</div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black"> Gender: {gender?.gender}</div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">Country: {country?.country[0]?.country_id}</div>
+      </div>
+    </div>
   );
 }
